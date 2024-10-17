ce9d5075bc1eaffb05fc84f21a460c8000a445a6
TINNOKA <mogesh26aue@gmail.com>
2024-10-17 19:44:55 +0530

    -a

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.getenv('SECRET')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://sparks1422.pythonanywhere.com','127.0.0.1','localhost']
+ALLOWED_HOSTS = ['https://sparks1422.pythonanywhere.com','127.0.0.1','localhost','sparks1422.pythonanywhere.com']
 
 
 # Application definition
