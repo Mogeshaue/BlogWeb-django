8d8ccaa1fa4dffe0d8444cf119d0504980659044
TINNOKA <mogesh26aue@gmail.com>
2024-11-05 20:19:05 +0530

    added html apge

--- blog/views.py
+++ blog/views.py
@@ -5,7 +5,6 @@ from django.shortcuts import render, get_object_or_404
 from .forms import PostForm
 from django.shortcuts import redirect   
 
-# Create your views here.
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
@@ -16,17 +15,24 @@ def post_detail(request, pk):
     return render(request, 'blog/post_detail.html', {'post': post})
 
 def post_new(request):
-    if request.method == "POST":
-        form = PostForm(request.POST)
-        if form.is_valid():
-            post = form.save(commit=False)
-            post.author = request.user
-            post.published_date = timezone.now()
-            post.save()
-            return redirect('post_detail', pk=post.pk)
+    print(request, 'request')
+    
+    if request.user.is_authenticated:
+        if request.method == "POST":
+            form = PostForm(request.POST)
+            if form.is_valid():
+                post = form.save(commit=False)
+                post.author = request.user
+                post.published_date = timezone.now()
+                post.save()
+                return redirect('post_detail', pk=post.pk)
+        else:
+            form = PostForm() 
+
+        return render(request, 'blog/post_edit.html', {'form': form})
     else:
-        form = PostForm()
-    return render(request, 'blog/post_edit.html', {'form': form})
+       
+        return render(request, 'blog/error.html')
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
